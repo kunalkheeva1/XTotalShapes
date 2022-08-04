cad5710ba9a5101dfd9b8cbeeb57c886e0ba8ec1
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-03 22:17:35 -0700

    created a regular method to detect the total shapes x creates

--- src/XTotalShapes.java
+++ src/XTotalShapes.java
@@ -1,6 +1,19 @@
 public class XTotalShapes {
 
-    
+    static int xTotalShapes(int [][]grid ){
+        int n = grid.length;
+        int m = grid[0].length;
+        int count =0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                count ++;
+                if(grid[i][j]=='X'){
+                    dfs(i,j, n, m, grid);
+                }
+            }
+        }
+    }
 
 
     public static void main(String[] args) {
