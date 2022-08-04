6e652616c04602a3761dc8964c9d527884e82e22
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-03 22:23:44 -0700

    coded a dfs method to detect other elements and create a wall of 0 in every search so that it cannot come back,

--- src/XTotalShapes.java
+++ src/XTotalShapes.java
@@ -1,5 +1,27 @@
 public class XTotalShapes {
 
+
+
+   static void dfs(int i, int j, int n, int m, int grid[][]){
+
+        if(i<0 ||j<0 || i==n || j==m){
+            return;
+        }
+
+        if(grid[i][j]== 'O'){
+            return;
+        }
+
+        grid[i][j] ='O';
+
+        dfs(i+1,j, n, m, grid);
+        dfs(i-1,j, n, m, grid);
+        dfs(i,j+1, n, m, grid);
+        dfs(i,j-1, n, m, grid);
+
+    }
+
+
     static int xTotalShapes(int [][]grid ){
         int n = grid.length;
         int m = grid[0].length;
@@ -12,7 +34,7 @@ public class XTotalShapes {
                     dfs(i,j, n, m, grid);
                 }
             }
-        }
+        }return count;
     }
 
 
